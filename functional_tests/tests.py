ca1d0a07c32b7bbb365a86bb5180b9a77c04be05
Ben Hutton <ben@benhutton.com.au>
2016-10-20 07:56:25 +0800

    added gecko driver support to selenium

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,29 @@ import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#firefox_capabilities = DesiredCapabilities.FIREFOX
+#firefox_capabilities['marionette'] = True
+#firefox_capabilities['binary'] = '/Users/myproject/geckodriver-0.8.0-OSX'
+
+caps = DesiredCapabilities.FIREFOX
+
+# Tell the Python bindings to use Marionette.
+# This will not be necessary in the future,
+# when Selenium will auto-detect what remote end
+# it is talking to.
+caps["marionette"] = True
+
+# Path to Firefox DevEdition or Nightly.
+# Firefox 47 (stable) is currently not supported,
+# and may give you a suboptimal experience.
+#
+# On Mac OS you must point to the binary executable
+# inside the application package, such as
+# /Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin
+#caps["binary"] = "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+
+driver = webdriver.Firefox(capabilities=caps)
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
@@ -23,8 +46,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+	#def tearDown(self):
+		#self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
@@ -37,9 +60,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser.get(self.server_url)
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
+		self.assertIn('To-Do lists', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+		self.assertIn('Start a new To-Do List', header_text)
 		
 		# She is invtied to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
